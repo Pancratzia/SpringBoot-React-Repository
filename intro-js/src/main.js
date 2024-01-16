b2e4ba6dd2690c25953e503eb171ed1150230bdd
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-15 21:58:01 -0400

    Objetos anidados y funciones

--- intro-js/src/main.js
+++ intro-js/src/main.js
@@ -2,15 +2,23 @@ const invoice = {
     id: 1,
     description: 'Compras de Aparatos Electronicos',
     date: new Date(),
-    client: "Pancratzia LaItaliana",
-    total: 500
+    client: {
+        id: 1,
+        name: 'Pancratzia',
+        lastname: 'LaItaliana',
+        age: 24,
+    },
+    total: 500,
+    greeting: function(){
+        return 'Hola! Me llamo ' + this.client.name
+    }
 }
 
 console.log(invoice)
 console.log(`
 FACTURA DEL COMERCIO DE JAVASCRIPT ELECTROTECH
 
-Cliente: ${invoice.client}
+Cliente: ${invoice.client.name} ${invoice.client.lastname}
 Fecha: ${invoice.date}
 Total: ${invoice.total}$
 
@@ -19,3 +27,4 @@ ${invoice.description}
 
 invoice.total = 1000
 console.log(`Los atributos del objeto se pueden modificar. Por ejemplo, el total ahora es de ${invoice.total}$`)
+console.log(invoice.greeting())
