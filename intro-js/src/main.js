5f4153d4caf5ace9e9c31210a4c7aa60d2f17701
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-15 22:02:58 -0400

    Asignando un arreglo de objetos

--- intro-js/src/main.js
+++ intro-js/src/main.js
@@ -8,7 +8,37 @@ const invoice = {
         lastname: 'LaItaliana',
         age: 24,
     },
-    total: 500,
+    items: [
+        {
+            product: 'keyboard',
+            price: 100,
+            quantity: 1
+        },
+        {
+            product: 'mouse',
+            price: 50,
+            quantity: 2
+        },
+        {
+            product: 'monitor',
+            price: 200,
+            quantity: 1
+        },
+        {
+            product: 'laptop',
+            price: 505,
+            quantity: 1
+        }
+    ],
+
+    total: function(){
+        let total = 0
+        for(let i = 0; i < this.items.length; i++){
+            total += this.items[i].price * this.items[i].quantity
+        }
+        return total;
+    },
+
     greeting: function(){
         return 'Hola! Me llamo ' + this.client.name
     }
@@ -20,11 +50,17 @@ FACTURA DEL COMERCIO DE JAVASCRIPT ELECTROTECH
 
 Cliente: ${invoice.client.name} ${invoice.client.lastname}
 Fecha: ${invoice.date}
-Total: ${invoice.total}$
+Total: ${invoice.total()}$
 
 ${invoice.description}
+
+DETALLES
+
+ITEM | PRECIO | CANTIDAD | SUBTOTAL
+
+${invoice.items.map(item => `${item.product} | ${item.price} | ${item.quantity} | ${item.price * item.quantity}`).join('\n')}
 `)
 
-invoice.total = 1000
-console.log(`Los atributos del objeto se pueden modificar. Por ejemplo, el total ahora es de ${invoice.total}$`)
+/*invoice.total = 1000
+console.log(`Los atributos del objeto se pueden modificar. Por ejemplo, el total ahora es de ${invoice.total}$`)*/
 console.log(invoice.greeting())
