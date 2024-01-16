94c61188154c18588823488159a644bf07cdb76f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-15 20:41:32 -0400

    Repaso de funciones finalizada

--- intro-js/src/main.js
+++ intro-js/src/main.js
@@ -14,8 +14,14 @@ function sayMyName(name = "JavaScript", respuesta = false) {
     console.log(`Un gusto. Mi nombre es ${name}! ${respuesta ? 'Es un placer conocerte.' : ''}`);
 }
 
+const question = (q) => console.log(q);
+const answer = (a) => `Mi profesión es ${a}!`;
+
+
 sayHi();
 const greeting = sayHiInReturn();
 console.log(greeting);
 sayMyName("Pancratzia");
 sayMyName(undefined, true);
+question("¿Cual es tu profesión?");
+console.log(answer("Programador Web"));
