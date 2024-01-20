e9e45aa1218b4a0aadddfb7be6f1c812e10239cc
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-20 15:11:04 -0400

    Usando React.createElement para crear elementos

--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const h1 = React.createElement('div', null, React.createElement('h1', null, 'Hola mundo, desde React'));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  h1
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*/
 )
