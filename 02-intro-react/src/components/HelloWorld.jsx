a46fe031f7a1634df982b7a525bcfe0c2aedaf24
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-20 19:02:39 -0400

    Pasando objetos como props y aprendiendo un poco más sobre que tipos de valores pueden pasarse por los mismos

--- 02-intro-react/src/components/HelloWorld.jsx
+++ 02-intro-react/src/components/HelloWorld.jsx
@@ -1,6 +1,11 @@
-
-export function HelloWorld({langName}) {
-    return <h1>Hello, {langName}!</h1>
+export function HelloWorld({ langName, id, title = "Hello World", fullName }) {
+  return (
+    <>
+      <div key={id}>
+        <h1>{title}</h1>
+        <h2>Estás aprendiendo sobre {langName}</h2>
+        <p>Creado por: {fullName.name + " " + fullName.lastname}</p>
+      </div>
+    </>
+  );
 }
-
-
--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -6,6 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld langName ={'React'} />
+    <HelloWorld langName ={'React'} id = {1} fullName = {{name: 'Pancratzia', lastname: 'LaItaliana'}} />
   </React.StrictMode>,
 )
