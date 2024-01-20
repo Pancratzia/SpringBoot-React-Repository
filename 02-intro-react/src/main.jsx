996a82235362e8319e921b3e5ca2a8c0b21ed9f2
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-20 15:16:39 -0400

    Creando el primer componente de React

--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { HelloWorld } from './components/HelloWorld'
+
 import './index.css'
 
-let h1 = React.createElement('div', null, React.createElement('h1', null, 'Hola mundo, desde React'));
-
-h1 = <div><ul><li>Adiós, Mundo!</li></ul></div>
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  h1
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>,*/
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
 )
