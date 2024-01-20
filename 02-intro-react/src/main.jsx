d6411cc35da1a329da2a9048aa526384f623d0e3
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-20 15:12:53 -0400

    Otra forma de crear elementos asignando una contante dichos valores html

--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const h1 = React.createElement('div', null, React.createElement('h1', null, 'Hola mundo, desde React'));
+let h1 = React.createElement('div', null, React.createElement('h1', null, 'Hola mundo, desde React'));
+
+h1 = <div><ul><li>Adiós, Mundo!</li></ul></div>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   h1
