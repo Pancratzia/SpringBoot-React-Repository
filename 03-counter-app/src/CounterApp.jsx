2cfdd685328fdfad2f4d94a5a436fad82f1c1cfd
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 10:35:29 -0400

    añadiendo props

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
-function CounterApp() {
-  const [counter, setCounter] = useState(0);
+function CounterApp({value}) {
+  const [counter, setCounter] = useState(value);
 
   const counterIncrement = () => {
     setCounter(counter + 1);
@@ -12,17 +13,25 @@ function CounterApp() {
   };
 
   const counterReset = () => {
-    setCounter(0);
+    setCounter(value);
   };
 
   return (
     <>
       <h2>El valor del contador es {counter}</h2>
       <button onClick={counterDecrement}>Contador --</button>
-      <button onClick={counterReset}>Contador = 0</button>
+      <button onClick={counterReset}>Contador = {value}</button>
       <button onClick={counterIncrement}>Contador ++</button>
     </>
   );
 }
 
 export default CounterApp;
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired
+}
+
+CounterApp.defaultProps = {
+  value: 0
+}
