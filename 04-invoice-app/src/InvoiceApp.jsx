cc56504cdc6a6222a51638f7569c78a7aca05671
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 13:26:27 -0400

    Dando estilos de bootstrap a la factura

--- 04-invoice-app/src/InvoiceApp.jsx
+++ 04-invoice-app/src/InvoiceApp.jsx
@@ -9,52 +9,70 @@ function InvoiceApp() {
   const { country, city, street, number } = address;
 
   return (
-    <>
-      <h1>Tu Factura</h1>
-      <ul>
-        <li>Id: {id}</li>
-        <li>Name: {name}</li>
-      </ul>
+    <div className="container">
+      <div className="card mt-5">
+        <div className="card-header">
+          <h1>Tu Factura</h1>
+        </div>
 
-      <h3>Datos del cliente</h3>
-      <ul>
-        <li>
-          Name: {clientName} {clientLastName}
-        </li>
-        <li>Country: {country}</li>
-        <li>City: {city}</li>
-        <li>
-          Street: {street} #{number}
-        </li>
-      </ul>
+        <div className="card-body">
+          <ul className="list-group">
+            <li className="list-group-item">Id: {id}</li>
+            <li className="list-group-item">Name: {name}</li>
+          </ul>
 
-      <h3>Datos de la empresa</h3>
-      <ul>
-        <li>Name: {companyName}</li>
-        <li>Fiscal Number: {fiscalNumber}</li>
-      </ul>
+          <div className="row my-3">
+            <div className="col">
+              <h3>Datos del cliente</h3>
+              <ul className="list-group">
+                <li className="list-group-item active">
+                  Name: {clientName} {clientLastName}
+                </li>
+                <li className="list-group-item">
+                  {country} / {city}
+                </li>
+                <li className="list-group-item">
+                  {street} #{number}
+                </li>
+              </ul>
+            </div>
 
-      <h4>Productos</h4>
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
+            <div className="col">
+              <h3>Datos de la empresa</h3>
+              <ul className="list-group">
+                <li className="list-group-item active">Name: {companyName}</li>
+                <li className="list-group-item">
+                  Fiscal Number: {fiscalNumber}
+                </li>
+              </ul>
+            </div>
+          </div>
 
-        <tbody>
-          {items.map(({ product, price, quantity, id }) => (
-            <tr key={id}>
-              <td>{product}</td>
-              <td>{price}</td>
-              <td>{quantity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+          <hr className="my-5" />
+
+          <h4>Productos</h4>
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {items.map(({ product, price, quantity, id }) => (
+                <tr key={id}>
+                  <td>{product}</td>
+                  <td>{price}</td>
+                  <td>{quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 }
 
