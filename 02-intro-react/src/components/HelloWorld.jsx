3ceaa567f75207315c237cef2d1c2fcb2f5c6b27
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-20 23:38:55 -0400

    Mejorando algunos estilos y estructura

--- 02-intro-react/src/components/HelloWorld.jsx
+++ 02-intro-react/src/components/HelloWorld.jsx
@@ -1,11 +1,13 @@
 export function HelloWorld({ langName, id, title = "Hello World", fullName }) {
+  const { name, lastname } = fullName;
+
   return (
-    <>
-      <div key={id}>
-        <h1>{title}</h1>
-        <h2>Estás aprendiendo sobre {langName}</h2>
-        <p>Creado por: {fullName.name + " " + fullName.lastname}</p>
-      </div>
-    </>
+    <div key={id} style={{ width: "95%", maxWidth: "1000px", margin: "20px auto", textAlign: "center" }}>
+      <h1>{title}</h1>
+      <h2>Estás aprendiendo sobre {langName}</h2>
+      <p>
+        Creado por: {name} {lastname}
+      </p>
+    </div>
   );
 }
