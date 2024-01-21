0fbc2d90c07e93fbf156fd34f762f37ac1dae4d6
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 16:16:23 -0400

    Calculando el total usando map y reduce

--- 04-invoice-app/src/services/getInvoice.js
+++ 04-invoice-app/src/services/getInvoice.js
@@ -1,10 +1,9 @@
-import { invoice } from '../data/invoice';
+import { invoice } from "../data/invoice";
 
 export const getInvoice = () => {
-    let total = 0;
-    invoice.items.forEach(item => {
-        total += item.price * item.quantity
-    });
+  const total = invoice.items
+    .map((item) => item.price * item.quantity)
+    .reduce((acumulador, item) => acumulador + item, 0);
 
-    return {...invoice, total};
-}
+  return { ...invoice, total };
+};
