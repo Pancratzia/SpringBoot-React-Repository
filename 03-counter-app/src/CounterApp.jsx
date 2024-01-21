24f331cce18d114decdaa251f94fb6604fe4d9cc
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 10:47:48 -0400

    Agregando estilos. Counter finalizado. Crece, decrece y resetea

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function CounterApp({value}) {
+function CounterApp({ value }) {
   const [counter, setCounter] = useState(value);
 
   const counterIncrement = () => {
@@ -17,21 +17,25 @@ function CounterApp({value}) {
   };
 
   return (
-    <>
-      <h2>El valor del contador es {counter}</h2>
-      <button onClick={counterDecrement}>Contador --</button>
-      <button onClick={counterReset}>Contador = {value}</button>
-      <button onClick={counterIncrement}>Contador ++</button>
-    </>
+    <div className="counterApp">
+      <div className="counter">
+        <h1>{counter}</h1>
+        <div className="buttons">
+          <button onClick={counterDecrement}>-</button>
+          <button onClick={counterReset}>Reset</button>
+          <button onClick={counterIncrement}>+</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default CounterApp;
 
 CounterApp.propTypes = {
-  value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 
 CounterApp.defaultProps = {
-  value: 0
-}
+  value: 0,
+};
