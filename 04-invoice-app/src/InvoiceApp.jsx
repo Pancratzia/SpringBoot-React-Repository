5e8cf92478040d897a1fdbf51b057a2d1092bb46
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 17:28:32 -0400

    Guardando los items creados

--- 04-invoice-app/src/InvoiceApp.jsx
+++ 04-invoice-app/src/InvoiceApp.jsx
@@ -8,12 +8,32 @@ import { getInvoice } from "./services/getInvoice";
 
 function InvoiceApp() {
   const invoice = getInvoice();
+  const { id, name, client, company, items: initialItems, total } = invoice;
 
   const [productValue, setProductValue] = useState("");
   const [priceValue, setPriceValue] = useState(0);
   const [quantityValue, setQuantityValue] = useState(0);
+  const [items, setItems] = useState(initialItems);
 
-  const { id, name, client, company, items, total } = invoice;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems([
+      ...items,
+      {
+        product: productValue,
+        price: priceValue,
+        quantity: quantityValue,
+        id: Date.now(),
+      },
+    ]);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setProductValue("");
+    setPriceValue(0);
+    setQuantityValue(0);
+  };
 
   return (
     <div className="container">
@@ -26,11 +46,11 @@ function InvoiceApp() {
           <InvoiceView id={id} name={name} />
           <div className="row my-3">
             <div className="col">
-              <ClientView client={client} title = {"Datos del cliente"} />
+              <ClientView client={client} title={"Datos del cliente"} />
             </div>
 
             <div className="col">
-             <CompanyView company={company} title = {"Datos de la Empresa"} />
+              <CompanyView company={company} title={"Datos de la Empresa"} />
             </div>
           </div>
 
@@ -39,12 +59,39 @@ function InvoiceApp() {
           <ListItemsView title="Items" items={items} />
           <TotalView total={total} />
 
-          <form className="mt-5">
-            <input type="text" name="product" placeholder="Producto..." className="form-control my-2" onChange={(e) => setProductValue(e.target.value)} value={productValue} />
-            <input type="number" name="price" placeholder="Precio ($)" className="form-control my-2" onChange={(e) => setPriceValue(e.target.value)} value={priceValue} />
-            <input type="number" name="quantity" placeholder="Cantidad" className="form-control my-2" onChange={(e) => setQuantityValue(e.target.value)} value={quantityValue} />
+          <form
+            className="mt-5 w-50"
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <input
+              type="text"
+              name="product"
+              placeholder="Producto..."
+              className="form-control my-2"
+              onChange={(e) => setProductValue(e.target.value)}
+              value={productValue}
+            />
+            <input
+              type="number"
+              name="price"
+              placeholder="Precio ($)"
+              className="form-control my-2"
+              onChange={(e) => setPriceValue(e.target.value)}
+              value={priceValue}
+            />
+            <input
+              type="number"
+              name="quantity"
+              placeholder="Cantidad"
+              className="form-control my-2"
+              onChange={(e) => setQuantityValue(e.target.value)}
+              value={quantityValue}
+            />
+
+            <button className="btn btn-primary">Añadir</button>
           </form>
-          
         </div>
       </div>
     </div>
