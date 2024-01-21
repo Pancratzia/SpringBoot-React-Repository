54ad33602c078ad1a89cf3e2ef3ab7aa870782cf
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 16:08:29 -0400

    Logrando que la factura tenga el total

--- 04-invoice-app/src/services/getInvoice.js
+++ 04-invoice-app/src/services/getInvoice.js
@@ -1,5 +1,10 @@
 import { invoice } from '../data/invoice';
 
 export const getInvoice = () => {
-    return invoice;
+    let total = 0;
+    invoice.items.forEach(item => {
+        total += item.price * item.quantity
+    });
+
+    return {...invoice, total};
 }
