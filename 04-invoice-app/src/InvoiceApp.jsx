52097d1036476b6a940161aca99d400fc224ae7f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-21 13:04:32 -0400

    Comenzando con el componente de la factura

--- 04-invoice-app/src/InvoiceApp.jsx
+++ 04-invoice-app/src/InvoiceApp.jsx
@@ -1,10 +1,17 @@
-
+import { getInvoice } from "./services/getInvoice";
 
 function InvoiceApp() {
+  const invoice = getInvoice();
 
   return (
-    <h1>Invoice App</h1>
-  )
+    <>
+      <h1>Tu Factura</h1>
+      <ul>
+        <li>Id: {invoice.id}</li>
+        <li>Name: {invoice.name}</li>
+      </ul>
+    </>
+  );
 }
 
-export default InvoiceApp
+export default InvoiceApp;
