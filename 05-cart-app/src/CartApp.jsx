b12d3830393c9ebdbc8d579e9dc0a42046cb6d92
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-24 17:14:53 -0400

    Creando la estructura HTML para el carrito de compras

--- 05-cart-app/src/CartApp.jsx
+++ 05-cart-app/src/CartApp.jsx
@@ -72,6 +72,38 @@ function CartApp() {
             </div>
           </div>
         </div>
+
+        <div className="my-4 w-75">
+
+          <h4>Carrito de Compras</h4>
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Precio ($)</th>
+                <th>Cantidad</th>
+                <th>Total ($)</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Nombre</td>
+                <td>0$</td>
+                <td>1</td>
+                <td>0$</td>
+                <td>Eliminar</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={3} className="text-end fw-bold">Total</td>
+                <td colSpan={2} className="fw-bold">0$</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+
       </div>
     </>
   );
