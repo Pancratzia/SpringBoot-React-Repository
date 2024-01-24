09a3bbc8ef29c763ee9cc987cbd09e809d8530d9
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-24 17:28:29 -0400

    Creando la data estatica de los productos

---
+++ 05-cart-app/src/data/productos.js
@@ -0,0 +1,39 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Laptop Acer Aspire',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 799.99,
+    },
+    {
+      id: 2,
+      name: 'PC Gaming HP Pavilion',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 1299.99,
+    },
+    {
+      id: 3,
+      name: 'Smartphone Samsung Galaxy S21',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 899.99,
+    },
+    {
+      id: 4,
+      name: 'Tablet Apple iPad Air',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 649.99,
+    },
+    {
+      id: 5,
+      name: 'Monitor LG UltraWide',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 449.99,
+    },
+    {
+      id: 6,
+      name: 'Cámara Sony Alpha A7III',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at dolor vitae libero scelerisque.',
+      price: 1999.99,
+    },
+  ];
+  
