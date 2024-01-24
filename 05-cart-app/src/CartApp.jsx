2faa5817e4b1851e3b64a4beb318ef5487d10f18
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-24 16:03:26 -0400

    Arreglando los lorem ipsum

--- 05-cart-app/src/CartApp.jsx
+++ 05-cart-app/src/CartApp.jsx
@@ -9,7 +9,7 @@ function CartApp() {
             <div className="card">
               <div className="card-body">
                 <h5 className="cart-title">Teclado Mecánico RGB</h5>
-                <p className="card-text">Teclado Mecanico con luces RGB y teclas tactiles</p>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">30$</p>
 
                 <button className="btn btn-primary">Comprar</button>
@@ -20,7 +20,7 @@ function CartApp() {
             <div className="card">
               <div className="card-body">
                 <h5 className="cart-title">Samsung Smart TV 4K</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">250$</p>
 
                 <button className="btn btn-primary">Comprar</button>
@@ -31,7 +31,7 @@ function CartApp() {
             <div className="card">
               <div className="card-body">
                 <h5 className="cart-title">Audifonos Bluetooth Sony</h5>
-                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, beatae.</p>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">35$</p>
 
                 <button className="btn btn-primary">Comprar</button>
@@ -42,7 +42,7 @@ function CartApp() {
             <div className="card">
               <div className="card-body">
                 <h5 className="cart-title">Monitor LG 24</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">100$</p>
 
                 <button className="btn btn-primary">Comprar</button>
@@ -52,8 +52,8 @@ function CartApp() {
           <div className="col-4 my-2">
             <div className="card">
               <div className="card-body">
-                <h5 className="cart-title">Mouse Gamer Logitech</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque.</p>
+                <h5 className="cart-title">Mouse Logitech</h5>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">10$</p>
 
                 <button className="btn btn-primary">Comprar</button>
@@ -64,7 +64,7 @@ function CartApp() {
             <div className="card">
               <div className="card-body">
                 <h5 className="cart-title">WebCam Logitech</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                 <p className="card-text">45$</p>
 
                 <button className="btn btn-primary">Comprar</button>
