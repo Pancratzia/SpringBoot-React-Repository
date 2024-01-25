aaa1f11ce84ada3bedd36d3997c355b6f4449f71
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-25 14:11:01 -0400

    Se agregan varios elementos al carrito de compras

--- 05-cart-app/src/CartApp.jsx
+++ 05-cart-app/src/CartApp.jsx
@@ -2,33 +2,44 @@ import { useState } from "react";
 import CartView from "./components/CartView";
 import CatalogView from "./components/CatalogView";
 
-const initialCartItems = []
-
+const initialCartItems = [];
 
 function CartApp() {
-
   const [cartItems, setCartItems] = useState(initialCartItems);
 
   const handlerAddProductCart = (product) => {
-    setCartItems([...cartItems, {
-      product: product,
-      quantity: 1,
-      price: product.price * 1
-    }]);
-  }
+    const hasItem = cartItems.find((item) => item.product.id === product.id);
+
+    if (hasItem) {
+      setCartItems([
+        ...cartItems.filter((item) => item.product.id !== product.id),
+        {
+          product,
+          quantity: hasItem.quantity + 1,
+        },
+      ]);
+    } else {
+      setCartItems([
+        ...cartItems,
+        {
+          product: product,
+          quantity: 1,
+          price: product.price * 1,
+        },
+      ]);
+    }
+  };
 
   return (
     <>
       <div className="container my-5">
         <h3>Cart App</h3>
 
-        <CatalogView handler={ (product) => handlerAddProductCart(product) } />
+        <CatalogView handler={(product) => handlerAddProductCart(product)} />
 
         <div className="my-4 w-75">
-
-          <CartView items = {cartItems} />
+          <CartView items={cartItems} />
         </div>
-
       </div>
     </>
   );
