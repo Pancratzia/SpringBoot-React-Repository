9df639c6b3540d1996450077438d116235762ea5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-27 12:22:55 -0400

    Maquetando el userForm y agregando manejo de estados con el useState

--- 06-users-app/src/components/UserForm.jsx
+++ 06-users-app/src/components/UserForm.jsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
+
+const initialUserform = {
+  username: "test",
+  password: "pass",
+  email: "a@a.a",
+};
+
 const UserForm = () => {
+  const [userForm, setUserForm] = useState(initialUserform);
+
+  const { username, password, email } = userForm;
+
+  const onInputChange = ({ target: { name, value } }) => {
+    setUserForm({ ...userForm, [name]: value });
+  };
+
   return (
-    <>
-      <p>Formulario de usuarios</p>
-    </>
+    <form>
+      <input
+        className="form-control my-3 w-75"
+        type="text"
+        placeholder="Username"
+        name="username"
+        onChange={onInputChange}
+        value={username}
+        required
+      />
+
+      <input
+        className="form-control my-3 w-75"
+        type="password"
+        placeholder="Password"
+        name="password"
+        onChange={onInputChange}
+        value={password}
+        required
+      />
+
+      <input
+        className="form-control my-3 w-75"
+        type="email"
+        placeholder="Email"
+        name="email"
+        onChange={onInputChange}
+        value={email}
+        required
+      />
+
+      <button className="btn btn-primary" type="submit">
+        Crear
+      </button>
+    </form>
   );
 };
 
