a2f92f72774782a28f79cba2f5f2dea16db0ec3b
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-28 14:13:03 -0400

    Creando el usersReducer

---
+++ 06-users-app/src/reducers/usersReducer.js
@@ -0,0 +1,15 @@
+export const usersReducer = (state = [], action) =>{
+
+    switch(action.type){
+        case "ADD_USER":
+            return [
+                ...state,
+                {
+                    ...action.payload,
+                    id: new Date().getTime(),
+                }
+            ];
+        default:
+            return state;
+    }
+}
