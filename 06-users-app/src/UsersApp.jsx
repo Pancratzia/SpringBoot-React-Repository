b380f4024d4a8fcee05930dcf3d7b128d0c083e1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-29 17:26:25 -0400

    Creando una ventana modal para el formulario

--- 06-users-app/src/UsersApp.jsx
+++ 06-users-app/src/UsersApp.jsx
@@ -17,23 +17,41 @@ function App() {
 
   return (
     <>
+      {visibleForm && (
+        <div className="abrir-modal animacion fadeIn">
+          <div className="modal" style={{ display: visibleForm ? "block" : "" }} tabIndex={-1}>
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">
+                    {userSelected.id ? "Edit User" : "New User"}
+                  </h5>
+                </div>
+
+                <div className="modal-body">
+                  <UserForm
+                    handlerAddUser={handlerAddUser}
+                    initialUserform={initialUserForm}
+                    userSelected={userSelected}
+                    handlerCloseForm={handlerCloseForm}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="container my-4">
         <h2>Users App</h2>
         <div className="row my-4">
-          {visibleForm && (
-            <div className="col">
-              <UserForm
-                handlerAddUser={handlerAddUser}
-                initialUserform={initialUserForm}
-                userSelected={userSelected}
-                handlerCloseForm={handlerCloseForm}
-              />
-            </div>
-          )}
-
           <div className="col">
             {!visibleForm && (
-              <button onClick={handlerOpenForm} className="btn btn-primary my-2">Nuevo Usuario</button>
+              <button
+                onClick={handlerOpenForm}
+                className="btn btn-primary my-2"
+              >
+                Nuevo Usuario
+              </button>
             )}
             <UsersList
               users={users}
