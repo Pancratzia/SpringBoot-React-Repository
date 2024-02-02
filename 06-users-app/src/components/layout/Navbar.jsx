4a87c864423c9c3534611b0a292545d28ad2ddbf
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-01 23:24:08 -0400

    Creando navbar-nav para los posibles links de la página

--- 06-users-app/src/components/layout/Navbar.jsx
+++ 06-users-app/src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 const Navbar = ({ login, handlerLogout }) => {
   return (
@@ -7,6 +8,15 @@ const Navbar = ({ login, handlerLogout }) => {
         <a className="navbar-brand" href="./">
           Users App
         </a>
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/users">
+              Usuarios
+            </NavLink>
+          </li>
+        </ul>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -18,13 +28,20 @@ const Navbar = ({ login, handlerLogout }) => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <span className="nav-item nav-link text-primary m-3">
-              Welcome {login.user?.username}
-            </span>
-            <button className="btn btn-outline-success m-3" onClick={handlerLogout}>
-                Logout
-            </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <span className="nav-item nav-link text-primary m-3">
+            Welcome {login.user?.username}
+          </span>
+          <button
+            className="btn btn-outline-success m-3"
+            onClick={handlerLogout}
+          >
+            Logout
+          </button>
         </div>
       </div>
     </nav>
