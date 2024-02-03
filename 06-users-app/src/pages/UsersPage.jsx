ff93ca47e2cccebcbd47f65e0085a2ed04528aea
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-03 10:04:33 -0400

    Usando useUsers en userRoutes

--- 06-users-app/src/pages/UsersPage.jsx
+++ 06-users-app/src/pages/UsersPage.jsx
@@ -1,20 +1,18 @@
-import { useUsers } from "../hooks/useUsers";
+import PropTypes from "prop-types";
 import UserModalForm from "../components/UserModalForm";
 import UsersList from "../components/UsersList";
 
-const UsersPage = () => {
-  
-    const {
-    users,
-    userSelected,
-    initialUserForm,
-    handlerAddUser,
-    handlerRemoveUser,
-    handlerUserSelectedForm,
-    visibleForm,
-    handlerOpenForm,
-    handlerCloseForm,
-  } = useUsers();
+const UsersPage = ({
+  users,
+  userSelected,
+  initialUserForm,
+  handlerAddUser,
+  handlerRemoveUser,
+  handlerUserSelectedForm,
+  visibleForm,
+  handlerOpenForm,
+  handlerCloseForm,
+}) => {
 
   return (
     <>
@@ -48,3 +46,15 @@ const UsersPage = () => {
 };
 
 export default UsersPage;
+
+UsersPage.propTypes = {
+  users: PropTypes.array.isRequired,
+  userSelected: PropTypes.object.isRequired,
+  initialUserForm: PropTypes.object.isRequired,
+  handlerAddUser: PropTypes.func.isRequired,
+  handlerRemoveUser: PropTypes.func.isRequired,
+  handlerUserSelectedForm: PropTypes.func.isRequired,
+  visibleForm: PropTypes.bool.isRequired,
+  handlerOpenForm: PropTypes.func.isRequired,
+  handlerCloseForm: PropTypes.func.isRequired,
+}
--- 06-users-app/src/routes/UserRoutes.jsx
+++ 06-users-app/src/routes/UserRoutes.jsx
@@ -1,28 +1,53 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import UsersPage from "../pages/UsersPage.jsx"
-import Navbar from "../components/layout/Navbar.jsx"
-import PropTypes from "prop-types"
-import RegisterPage from "../pages/RegisterPage.jsx"
+import { Routes, Route, Navigate } from "react-router-dom";
+import UsersPage from "../pages/UsersPage.jsx";
+import Navbar from "../components/layout/Navbar.jsx";
+import PropTypes from "prop-types";
+import RegisterPage from "../pages/RegisterPage.jsx";
+import { useUsers } from "../hooks/useUsers.js";
 
-const UserRoutes = ({ login, handlerLogout}) => {
+const UserRoutes = ({ login, handlerLogout }) => {
+  const {
+    users,
+    userSelected,
+    initialUserForm,
+    handlerAddUser,
+    handlerRemoveUser,
+    handlerUserSelectedForm,
+    visibleForm,
+    handlerOpenForm,
+    handlerCloseForm,
+  } = useUsers();
+
   return (
     <>
-
-        <Navbar login={login} handlerLogout={handlerLogout}  />
-        <Routes>
-
-            <Route path="users" element={<UsersPage />} />
-            <Route path="users/register" element={<RegisterPage />} />
-            <Route path="/" element={<Navigate to="/users" />} />
-
-        </Routes>
+      <Navbar login={login} handlerLogout={handlerLogout} />
+      <Routes>
+        <Route
+          path="users"
+          element={
+            <UsersPage
+              users={users}
+              userSelected={userSelected}
+              initialUserForm={initialUserForm}
+              visibleForm={visibleForm}
+              handlerAddUser={handlerAddUser}
+              handlerRemoveUser={handlerRemoveUser}
+              handlerUserSelectedForm={handlerUserSelectedForm}
+              handlerOpenForm={handlerOpenForm}
+              handlerCloseForm={handlerCloseForm}
+            />
+          }
+        />
+        <Route path="users/register" element={<RegisterPage handlerAddUser={handlerAddUser} initialUserForm={initialUserForm} />} />
+        <Route path="/" element={<Navigate to="/users" />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
 export default UserRoutes;
 
 UserRoutes.propTypes = {
-    login: PropTypes.object.isRequired,
-    handlerLogout: PropTypes.func.isRequired
-}
+  login: PropTypes.object.isRequired,
+  handlerLogout: PropTypes.func.isRequired,
+};
