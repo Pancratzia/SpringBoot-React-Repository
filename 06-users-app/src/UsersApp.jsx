841d347a499ee8124e15c41bb513d275037dcf8a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-05 19:47:32 -0400

    Empezando a implementar el AuthContext

--- 06-users-app/src/UsersApp.jsx
+++ 06-users-app/src/UsersApp.jsx
@@ -1,10 +1,12 @@
 import LoginPage from "./auth/pages/LoginPage.jsx";
-import { useAuth } from "./auth/hooks/useAuth.js";
 import { Routes, Route, Navigate } from "react-router-dom";
 import UserRoutes from "./routes/UserRoutes.jsx";
+import { useContext } from "react";
+import { AuthContext } from "./auth/context/AuthContext.jsx";
 
 function UsersApp() {
-  const { login, handlerLogin, handlerLogout } = useAuth();
+  
+  const { login } = useContext(AuthContext);
 
   return (
     <Routes>
--- 06-users-app/src/auth/context/AuthProvider.jsx
+++ 06-users-app/src/auth/context/AuthProvider.jsx
@@ -1,10 +1,17 @@
+import { useAuth } from "../hooks/useAuth";
 import { AuthContext } from "./AuthContext"
 import PropTypes from "prop-types";
 
 export const AuthProvider = ({ children }) => {
 
+    const { login, handlerLogin, handlerLogout } = useAuth();
+
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{
+            login,
+            handlerLogin,
+            handlerLogout
+        }}>
             {children}
         </AuthContext.Provider>
     )
