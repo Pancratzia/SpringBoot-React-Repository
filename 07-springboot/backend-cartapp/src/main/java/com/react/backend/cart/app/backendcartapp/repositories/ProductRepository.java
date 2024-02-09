e135d3651380e30e57b3b3d5b248124f40edad53
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-08 22:54:08 -0400

    Se vuelve al nombre original de repositorio por buenas practicas

--- 07-springboot/backend-cartapp/src/main/java/com/react/backend/cart/app/backendcartapp/repositories/ProductRepository.java
+++ 07-springboot/backend-cartapp/src/main/java/com/react/backend/cart/app/backendcartapp/repositories/ProductRepository.java
@@ -2,5 +2,5 @@ package com.react.backend.cart.app.backendcartapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import com.react.backend.cart.app.backendcartapp.models.entities.Product;
 
-public interface productRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
