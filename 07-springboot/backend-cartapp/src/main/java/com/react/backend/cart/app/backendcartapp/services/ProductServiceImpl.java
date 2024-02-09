ed0a5ed84ebd158c6259d9500dc939f6ea518895
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-08 23:00:38 -0400

    Agregando el transactional

--- 07-springboot/backend-cartapp/src/main/java/com/react/backend/cart/app/backendcartapp/services/ProductServiceImpl.java
+++ 07-springboot/backend-cartapp/src/main/java/com/react/backend/cart/app/backendcartapp/services/ProductServiceImpl.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.react.backend.cart.app.backendcartapp.models.entities.Product;
 import com.react.backend.cart.app.backendcartapp.repositories.ProductRepository;
@@ -14,6 +15,7 @@ public class ProductServiceImpl implements ProductService {
     private ProductRepository repository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findAll() {
         return (List<Product>)repository.findAll();
     }
