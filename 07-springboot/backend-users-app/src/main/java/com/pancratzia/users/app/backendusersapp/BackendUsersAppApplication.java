8c62982035a8376dac5ca80a9017ef4247bd55f3
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-10 10:51:37 -0400

    Inicializando el backend de la aplicacion de usuarios

---
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/BackendUsersAppApplication.java
@@ -0,0 +1,13 @@
+package com.pancratzia.users.app.backendusersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsersAppApplication.class, args);
+	}
+
+}
---
+++ 07-springboot/backend-users-app/src/test/java/com/pancratzia/users/app/backendusersapp/BackendUsersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pancratzia.users.app.backendusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
