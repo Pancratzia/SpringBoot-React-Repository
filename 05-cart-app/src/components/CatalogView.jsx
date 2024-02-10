65ee402d527b8d059040bd6a27c007fe6b852c26
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-10 10:43:10 -0400

    Agregando un loader

--- 05-cart-app/src/components/CatalogView.jsx
+++ 05-cart-app/src/components/CatalogView.jsx
@@ -5,36 +5,41 @@ import ProductCardVie from "./ProductCardView";
 
 const CatalogView = ({ handler }) => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const findAll = async () => {
     const data = await getProducts();
     setProducts(data);
-  }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     findAll();
   }, []);
 
   return (
-    <div className="row">
-      {products.length > 0 &&
-        products.map((product) => (
-          <div className="col-4 my-2" key={product.id}>
-            <ProductCardVie
-              handler = { handler }
-              id={product.id}
-              name={product.name}
-              description={product.description}
-              price={product.price}
-            />
-          </div>
-        ))}
-    </div>
+    <>
+      {isLoading && <div className="alert alert-info">Loading...</div>}
+      <div className="row">
+        {products.length > 0 &&
+          products.map((product) => (
+            <div className="col-4 my-2" key={product.id}>
+              <ProductCardVie
+                handler={handler}
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+              />
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
 export default CatalogView;
 
 CatalogView.propTypes = {
-  handler: PropTypes.func.isRequired
-}
+  handler: PropTypes.func.isRequired,
+};
