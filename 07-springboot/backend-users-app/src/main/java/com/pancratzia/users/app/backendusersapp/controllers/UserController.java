f8d30b994d6ec8a9d017eacf00bcb5385bdd92d4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-25 16:53:09 -0400

    Quick changes

--- 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/controllers/UserController.java
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/controllers/UserController.java
@@ -4,17 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pancratzia.users.app.backendusersapp.models.entities.User;
 import com.pancratzia.users.app.backendusersapp.services.UserService;
-
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.validation.BindingResult;
-
-
 import org.springframework.http.HttpStatus;
 
 @RestController
@@ -82,7 +75,6 @@ public class UserController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
-
     }
 
     private ResponseEntity<?> validation(BindingResult result) {
