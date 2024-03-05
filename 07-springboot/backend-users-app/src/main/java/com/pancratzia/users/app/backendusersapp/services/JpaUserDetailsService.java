bd28b3fb05b8384cc6253271c5fc09ff020bda90
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-05 17:30:10 -0400

    Creando un JpaUserDetailsService base para comprobar el funcionamiento de la seguridad

---
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/services/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.pancratzia.users.app.backendusersapp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!username.equals("admin")) {
+            throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema", username));
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new User(username, "12345", true, true, true, true, authorities);
+    }
+
+}
