e39a4e7992202e7a60364bb42bd6298630f1dcc7
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-05 16:48:14 -0400

    Configurando la configuración de seguridad de Spring

---
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.pancratzia.users.app.backendusersapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @SuppressWarnings("removal")
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.GET, "/users").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .csrf(config -> config.disable())
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+    };
+
+}
