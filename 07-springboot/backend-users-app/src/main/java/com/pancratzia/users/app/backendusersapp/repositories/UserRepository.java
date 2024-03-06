7c00f0a3e41aa71647c12a2833001c0fe8a67628
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-06 18:20:21 -0400

    Iniciando sesión con el usuario que está en la BD

--- 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/repositories/UserRepository.java
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/repositories/UserRepository.java
@@ -1,9 +1,17 @@
 package com.pancratzia.users.app.backendusersapp.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pancratzia.users.app.backendusersapp.models.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+
+    Optional<User> findByUsername(String username); //Metodo 1
+
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    Optional<User> getUserByUsername(String username); //Metodo 2
 }
