8441d46e011907d5f92a5f26754b14317fcf23ce
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-06 17:54:53 -0400

    Solucionando bug al validar el token con regex

--- 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/auth/filters/JwtAuthenticationFilter.java
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/auth/filters/JwtAuthenticationFilter.java
@@ -61,7 +61,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
                 String username = ((org.springframework.security.core.userdetails.User) authResult.getPrincipal()).getUsername();
 
-                String originalInput = SECRET_KEY + username;
+                String originalInput = SECRET_KEY + "." + username;
                 String token = Base64.getEncoder().encodeToString(originalInput.getBytes());
 
                 response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
