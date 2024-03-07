7e86fb06238ccbdd1f0ccb54f121566a998ae52c
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-07 15:44:34 -0400

    Optimizando el DtoMapperUser

--- 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/models/dto/mapper/DtoMapperUser.java
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/models/dto/mapper/DtoMapperUser.java
@@ -5,24 +5,23 @@ import com.pancratzia.users.app.backendusersapp.models.entities.User;
 
 public class DtoMapperUser {
 
-    private static DtoMapperUser mapper;
-
     private User user;
 
-    private DtoMapperUser() {}
+    private DtoMapperUser() {
+    }
 
     public static DtoMapperUser builder() {
-        mapper = new DtoMapperUser();
-        return mapper;
+        return new DtoMapperUser();
+
     }
 
-    public DtoMapperUser setUser (User user) {
+    public DtoMapperUser setUser(User user) {
         this.user = user;
-        return mapper;
+        return this;
     }
 
-    public UserDto build(){
-        if(user == null){
+    public UserDto build() {
+        if (user == null) {
             throw new RuntimeException("Entity User is null");
         }
         return new UserDto(this.user.getId(), this.user.getUsername(), this.user.getEmail());
