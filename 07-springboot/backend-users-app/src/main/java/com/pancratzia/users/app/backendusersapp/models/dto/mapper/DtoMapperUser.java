eb0b3d7a5c2a2c5b6ade17c384356b0360b624a9
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-07 15:31:32 -0400

    Terminando el DtoMapperUser

--- 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/models/dto/mapper/DtoMapperUser.java
+++ 07-springboot/backend-users-app/src/main/java/com/pancratzia/users/app/backendusersapp/models/dto/mapper/DtoMapperUser.java
@@ -1,13 +1,31 @@
 package com.pancratzia.users.app.backendusersapp.models.dto.mapper;
 
+import com.pancratzia.users.app.backendusersapp.models.dto.UserDto;
+import com.pancratzia.users.app.backendusersapp.models.entities.User;
+
 public class DtoMapperUser {
 
     private static DtoMapperUser mapper;
 
+    private User user;
+
     private DtoMapperUser() {}
 
-    public static DtoMapperUser getInstance() {
+    public static DtoMapperUser builder() {
         mapper = new DtoMapperUser();
         return mapper;
+    }
+
+    public DtoMapperUser setUser (User user) {
+        this.user = user;
+        return mapper;
+    }
+
+    public UserDto build(){
+        if(user == null){
+            throw new RuntimeException("Entity User is null");
+        }
+        return new UserDto(this.user.getId(), this.user.getUsername(), this.user.getEmail());
+
     }
 }
