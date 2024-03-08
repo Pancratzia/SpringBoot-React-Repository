1c2fcd095e9256540348d5e31ded7d5f1dd1dd63
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-08 18:12:02 -0400

    Se termina de crear el slice de Auth

--- 06-users-app/src/store/slices/auth/authSlice.js
+++ 06-users-app/src/store/slices/auth/authSlice.js
@@ -10,11 +10,17 @@ export const authSlice = createSlice({
     name: "auth",
     initialState: initialLogin,
     reducers: {
-        ON_LOGIN: (state, action) => {
-            
+        ON_LOGIN: (state, {payload}) => {
+            state.isAuth = true;
+            state.isAdmin = payload.isAdmin;
+            state.user = payload.user;
         },
-        ON_LOGOUT: (state, action) => {
-            
+        ON_LOGOUT: (state) => {
+            state.isAuth = false;
+            state.isAdmin = false;
+            state.user = undefined;
         }
     }
 });
+
+export const { ON_LOGIN, ON_LOGOUT } = authSlice.actions;
