879c753b5d492aef32d85f491d689dfaf62c3184
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-08 21:12:02 -0400

    Usando variables de ambiente para la BASE URL

--- 06-users-app/src/apis/usersApi.js
+++ 06-users-app/src/apis/usersApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const usersApi = axios.create({
-    baseURL: "http://localhost:8080/users",
+    baseURL: `${import.meta.env.VITE_API_URL}/users`,
 });
 
 usersApi.interceptors.request.use((config) => {
